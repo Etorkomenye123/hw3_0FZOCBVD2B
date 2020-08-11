fde4fb4a591febfb96be47b53e8e0ada38e9020a
Thywill <attipoethywill71@gmail.com>
2020-08-11 12:57:08 -0700

    Initial commit
    Week 3 Assignment

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Navigation from './navigation.js';
+import Content from './content.js';
+
+function App() {
+  return (
+    <div>
+      <Navigation/>
+      <Content/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/navigation.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import'./css/layout.css';
+import'./css/global.css';
+
+function Navigation () {
+    return (
+        <nav class="navbar">
+        <div class="container">
+          <a href="#">Hulk</a>
+          <a href="#" class="logo is-active">Mavel</a>
+          <a href="#">Venom</a>
+        </div>
+      </nav>
+    
+    )
+}
+
+export default Navigation
